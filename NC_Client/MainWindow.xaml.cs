5605926264bdabc8a8f35b6d2fb60e129d3e5675
Protanker25 <Protanker25@gmail.com>
2020-11-18 03:51:11 +1000

    Разбил главный класс на 2 файла

--- NC_Client/MainWindow.xaml.cs
+++ NC_Client/MainWindow.xaml.cs
@@ -55,73 +55,6 @@ namespace NC_Client
         #endregion
 
 
-        #region Metods
-        void SaveSceneFile(string path, Scene scene)
-        {
-            using(var fs = File.Open(path, FileMode.OpenOrCreate))
-            {
-                string ser_scene = JsonSerializer.Serialize(scene);
-                using(var writer = new StreamWriter(fs))
-                {
-                    writer.Write(ser_scene);
-                }
-            }
-        }
-        Scene LoadSceneFile(string path)
-        {
-            using (FileStream fs = File.Open(path, FileMode.Open))
-            {
-                using (var reader = new StreamReader(fs))
-                {
-                    string file = reader.ReadToEnd();
-                    return JsonSerializer.Deserialize<Scene>(file);
-                }
-            }
-        }
-
-        SettingsFile LoadConfig()
-        {
-            try
-            {
-                using (FileStream fs = new FileStream("config.json", FileMode.Open, FileAccess.Read))
-                {
-                    using (var stream = new StreamReader(fs))
-                    {
-                        string load_config = stream.ReadToEnd();
-                        return JsonSerializer.Deserialize<SettingsFile>(load_config);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            return new SettingsFile() { Window_Width = Properties.Settings.Default.WindowWidth,
-                Window_Height = Properties.Settings.Default.WindowHeight
-            };
-        }
-        void SaveConfig(SettingsFile file)
-        {
-            string save_config = JsonSerializer.Serialize<SettingsFile>(file);
-            try
-            {
-                using (FileStream fs = new FileStream("config.json", FileMode.OpenOrCreate, FileAccess.Write))
-                {
-                    using (var stream = new StreamWriter(fs))
-                    {
-                        stream.Write(save_config);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        #endregion
-
-
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             ChangeFrame(curr_scene, curr_frame++%curr_scene.Length);
@@ -233,38 +166,6 @@ namespace NC_Client
         //        }
         //    }
         //}
-        void ChangeFrame(Scene scene, int frame)
-        {
-
-            string frame_backgroud = scene[frame].background;
-            BitmapImage background_image = resourses.GetBackground(frame_backgroud);
-            BackgroundImage.Source = background_image;
-            foreach(var character in scene[frame].sprites)
-            {
-                Character.SetImage(resourses.GetCharacter(character.Key),
-                    character.Value);
-            }
-            ShowText(frame, 25);
-        }
-        async void ShowText(int frame, int time_del)
-        {
-            FrameText.Text = "";
-            skip = false;
-            foreach (char sign in curr_scene[frame].text)
-            {
-                if(skip)
-                {
-                    FrameText.Text = curr_scene[frame].text;
-                    break;
-                }
-                
-                FrameText.Text += sign;
-                if (sign == ' ') continue;
-                await Task.Delay(time_del);
-            }
-            skip = true;
-        }
-
         private void ClickHandler_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             if(skip == false)
---
+++ NC_Client/MainWindow_2.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.DirectoryServices;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Ionic.Zip;
+using System.Drawing;
+using Point = System.Drawing.Point;
+
+namespace NC_Client
+{
+    public partial class MainWindow : Window
+    {
+        void ChangeFrame(Scene scene, int frame)
+        {
+
+            string frame_backgroud = scene[frame].background;
+            BitmapImage background_image = resourses.GetBackground(frame_backgroud);
+            BackgroundImage.Source = background_image;
+            foreach (var character in scene[frame].sprites)
+            {
+                Character.SetImage(resourses.GetCharacter(character.Key),
+                    character.Value);
+            }
+            ShowText(frame, 25);
+        }
+        async void ShowText(int frame, int time_del)
+        {
+            FrameText.Text = "";
+            skip = false;
+            foreach (char sign in curr_scene[frame].text)
+            {
+                if (skip)
+                {
+                    FrameText.Text = curr_scene[frame].text;
+                    break;
+                }
+
+                FrameText.Text += sign;
+                if (sign == ' ') continue;
+                await Task.Delay(time_del);
+            }
+            skip = true;
+        }
+        void SaveSceneFile(string path, Scene scene)
+        {
+            using (var fs = File.Open(path, FileMode.OpenOrCreate))
+            {
+                string ser_scene = JsonSerializer.Serialize(scene);
+                using (var writer = new StreamWriter(fs))
+                {
+                    writer.Write(ser_scene);
+                }
+            }
+        }
+        Scene LoadSceneFile(string path)
+        {
+            using (FileStream fs = File.Open(path, FileMode.Open))
+            {
+                using (var reader = new StreamReader(fs))
+                {
+                    string file = reader.ReadToEnd();
+                    return JsonSerializer.Deserialize<Scene>(file);
+                }
+            }
+        }
+        SettingsFile LoadConfig()
+        {
+            try
+            {
+                using (FileStream fs = new FileStream("config.json", FileMode.Open, FileAccess.Read))
+                {
+                    using (var stream = new StreamReader(fs))
+                    {
+                        string load_config = stream.ReadToEnd();
+                        return JsonSerializer.Deserialize<SettingsFile>(load_config);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            return new SettingsFile()
+            {
+                Window_Width = Properties.Settings.Default.WindowWidth,
+                Window_Height = Properties.Settings.Default.WindowHeight
+            };
+        }
+        void SaveConfig(SettingsFile file)
+        {
+            string save_config = JsonSerializer.Serialize<SettingsFile>(file);
+            try
+            {
+                using (FileStream fs = new FileStream("config.json", FileMode.OpenOrCreate, FileAccess.Write))
+                {
+                    using (var stream = new StreamWriter(fs))
+                    {
+                        stream.Write(save_config);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
